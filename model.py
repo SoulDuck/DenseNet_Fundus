8b2a25cb9245459f280f795347e3fd0021568b13
kimseongjung <plznw4me@naver.com>
2017-10-08 12:38:36 +0900

    for fundus data set | DenseNet

--- model.py
+++ model.py
@@ -290,7 +290,7 @@ class DenseNet:
         for i in range(max_iter):
             batch_xs ,batch_ys , batch_fs = self.get_batches_from_tensor(sess=self.sess  , images=self._images_tensor_list,\
                                                                          labels=self._labels_tensor_list , filenames=self._fnames_tensor_list )
-            batch_ys=input.cls_to_onehot(batch_ys , self.n_classes )
+            #batch_ys=input.cls_to_onehot(batch_ys , self.n_classes )
             print batch_ys
             feed_dict = {
                 #self._images_tensor_list , self._labels_tensor_list , self._fnames_tensor_list
